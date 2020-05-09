4964c9dc43caa34eebd9ce840ab470d9b6c1edbf
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2020-05-09 15:44:19 +0200

    [ADD] game state enum and globaleventhandler

---
+++ Classes/Utils/Enums/GameState.cs
@@ -0,0 +1,25 @@
+﻿namespace fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Utils.Enums
+{
+    /// <summary>
+    /// Enum used for know the game state
+    /// </summary>
+    public enum GameState
+    {
+        /// <summary>
+        /// In menu state
+        /// </summary>
+        MENU = 0,
+        /// <summary>
+        /// Loading state
+        /// </summary>
+        LOADING = 3,
+        /// <summary>
+        /// In game state
+        /// </summary>
+        IN_GAME = 2,
+        /// <summary>
+        /// Pause state
+        /// </summary>
+        PAUSE = 4
+    }
+}
