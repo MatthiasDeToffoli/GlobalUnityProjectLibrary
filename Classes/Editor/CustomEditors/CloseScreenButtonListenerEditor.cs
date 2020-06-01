02c06b48d78dd515681803d8085918025fefb619
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2020-06-01 20:47:28 +0200

    [ADD] close screen button listener

---
+++ Classes/Editor/CustomEditors/CloseScreenButtonListenerEditor.cs
@@ -0,0 +1,89 @@
+﻿using fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Menu;
+using fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Menu.ButtonListeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.PersonalEditors.CustomEditors
+{
+    /// <summary>
+    /// Custom Editor for the close menu button listener class
+    /// </summary>
+    /// <seealso cref="Editor"/> 
+    /// <seealso cref="CloseScreenButtonListener"/>
+    [CustomEditor(typeof(CloseScreenButtonListener),true)]
+    class CloseScreenButtonListenerEditor : Editor
+    {
+        #region Constants
+        /// <summary>
+        /// string of the variale mScreenToCloseIsParent
+        /// </summary>
+        private const string SCREEN_TO_CLOSE_IS_PARENT = "mScreenToCloseIsParent";
+
+        /// <summary>
+        /// the title show in inspector for the screen to close is parent property
+        /// </summary>
+        private const string SCREEN_TO_CLOSE_IS_PARENT_TITLE = "Screen to close is parent ?";
+        /// <summary>
+        /// string of the variale screenToClose
+        /// </summary>
+        private const string SCREEN_TO_CLOSE = "mScreenToClose";
+
+        /// <summary>
+        /// the title show in inspector for the screen to close property
+        /// </summary>
+        private const string SCREEN_TO_CLOSE_TITLE = "Screen to close";
+        #endregion Constants
+
+        #region Fields
+        /// <summary>
+        /// The screen to close is parent serialized property
+        /// </summary>
+        private SerializedProperty mScreenToCloseIsParentProperty;
+
+        /// <summary>
+        /// The screen to close serialized property
+        /// </summary>
+        private SerializedProperty mScreenToCloseProperty;
+        #endregion Fields
+
+        #region Methods
+        /// <summary>
+        /// When the GUI is enable
+        /// </summary>
+        protected virtual void OnEnable()
+        {
+            //we get the property
+            mScreenToCloseIsParentProperty = serializedObject.FindProperty(SCREEN_TO_CLOSE_IS_PARENT);
+            mScreenToCloseProperty = serializedObject.FindProperty(SCREEN_TO_CLOSE);
+        }
+        /// <summary>
+        /// When the inspector is updated
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            CreateProperties();
+
+            if (EditorGUI.EndChangeCheck())
+                serializedObject.ApplyModifiedProperties();
+
+
+        }
+
+        /// <summary>
+        /// Create all the properties
+        /// </summary>
+        protected virtual void CreateProperties()
+        {
+            EditorGUILayout.PropertyField(mScreenToCloseIsParentProperty, new GUIContent(SCREEN_TO_CLOSE_IS_PARENT_TITLE));
+
+            //we show the screen to close in function of the boolean value
+            if (!mScreenToCloseIsParentProperty.boolValue)
+            {
+                mScreenToCloseProperty.objectReferenceValue = EditorGUILayout.ObjectField(SCREEN_TO_CLOSE_TITLE, mScreenToCloseProperty.objectReferenceValue, typeof(AMenuScreen), true);
+            }
+        }
+        #endregion Methods
+    }
+}
---
+++ Classes/Menu/ButtonListeners/CloseScreenButtonListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace fr.matthiasdetoffoli.GlobalUnityProjectCode.Classes.Menu.ButtonListeners
+{
+    /// <summary>
+    /// Class used for make a button close a screen
+    /// </summary>
+    /// <seealso cref="AButtonListener"/>
+    public class CloseScreenButtonListener : AButtonListener
+    {
+        #region Fields
+        /// <summary>
+        /// If the screen to close is the parent of the button or another screen
+        /// </summary>
+        [SerializeField]
+        private bool mScreenToCloseIsParent;
+
+        /// <summary>
+        /// the screen to close
+        /// </summary>
+        /// <remarks>if it's the parent the property is not used</remarks>
+        [SerializeField]
+        private AMenuScreen mScreenToClose;
+        #endregion //Fields
+
+        #region Methods
+        /// <summary>
+        /// call when we click on the button
+        /// </summary>
+        protected override void OnButtonClicked()
+        {
+            if (mScreenToCloseIsParent)
+            {
+                //if the screen to close is parent so close the parent
+                gameObject.GetComponentInParent<AMenuScreen>()?.Close();
+            }
+            else
+            {
+                //else close the screen to close
+                mScreenToClose?.Close();
+            }
+        }
+        #endregion Methods
+    }
+}
